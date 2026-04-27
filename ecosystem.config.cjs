9ed77f11a5fe50615701829da5dd352cc26bdeee
Niranjan-M-T <a2zvideos1765@gmail.com>
2026-04-27 15:27:16 +0530

    config file dir change

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'finance-tracker-api',
       script: 'server.js',
-      cwd: '/home/user/finance-tracker-api',
+      cwd: './',
       instances: 1,
       autorestart: true,
       watch: false,
@@ -13,8 +13,8 @@ module.exports = {
         PORT: 3500
       },
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      error_file: '/home/user/finance-tracker-api/logs/pm2-error.log',
-      out_file: '/home/user/finance-tracker-api/logs/pm2-out.log',
+      error_file: './logs/pm2-error.log',
+      out_file: './logs/pm2-out.log',
       merge_logs: true
     }
   ]
